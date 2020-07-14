64042b942d93de5d32fedd4cc14f320b97c73fc7
HaripriyaMehta <hmehta@mit.edu>
2020-07-14 01:29:37 -0500

    Adding assets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "assets/css/bootstrap.min.css";
+import "assets/css/paper-kit.css";
+import "assets/demo/demo.css";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
