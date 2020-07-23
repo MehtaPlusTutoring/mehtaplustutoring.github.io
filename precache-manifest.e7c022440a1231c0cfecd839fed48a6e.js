66bd2e30b2c2f1dfc8bf383281256b1d467e3748
HaripriyaMehta <hmehta@mit.edu>
2020-07-23 09:00:55 -0500

    Updates

---
+++ precache-manifest.e7c022440a1231c0cfecd839fed48a6e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fa5505688338351be435b01c76548b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0e051af28290e61c69",
+    "url": "/static/css/main.452aacd8.chunk.css"
+  },
+  {
+    "revision": "ff18884afe164d6b5b14",
+    "url": "/static/js/2.6a644933.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6a644933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0e051af28290e61c69",
+    "url": "/static/js/main.7f65ecde.chunk.js"
+  },
+  {
+    "revision": "2dc06b5a53a45eeabfeb",
+    "url": "/static/js/runtime-main.15898951.js"
+  },
+  {
+    "revision": "8f2761a99b75cca41378fe79830dde11",
+    "url": "/static/media/andrea.8f2761a9.png"
+  },
+  {
+    "revision": "1877661d5f223a1858d26fdef54b15f9",
+    "url": "/static/media/bhagirath.1877661d.png"
+  },
+  {
+    "revision": "20d50d52a08b9b2ef8f8cd6c9f14afb5",
+    "url": "/static/media/classml20pic.20d50d52.png"
+  },
+  {
+    "revision": "5d786e99d258afda104a61f41a1537df",
+    "url": "/static/media/gmail.5d786e99.png"
+  },
+  {
+    "revision": "7b9f7df9ac89ef879846f84a59acad53",
+    "url": "/static/media/haripriya.7b9f7df9.png"
+  },
+  {
+    "revision": "4e0f028d234b4c780638de08cdda5263",
+    "url": "/static/media/instagram.4e0f028d.png"
+  },
+  {
+    "revision": "3db0562767b0aa9391c842ff9a65c136",
+    "url": "/static/media/marwa.3db05627.jpg"
+  },
+  {
+    "revision": "1b946cd84e2ac76c05f027d02f2db0e4",
+    "url": "/static/media/mehtapluslogo.1b946cd8.png"
+  },
+  {
+    "revision": "d3be59fc95e6d0a941a605746e0f1607",
+    "url": "/static/media/minilogo.d3be59fc.jpg"
+  },
+  {
+    "revision": "7eef11102ba8fe90421ca803d865caec",
+    "url": "/static/media/ml_world2020.7eef1110.png"
+  },
+  {
+    "revision": "dc2a59ce7ad1599ac04b53aacd655959",
+    "url": "/static/media/twitter.dc2a59ce.png"
+  },
+  {
+    "revision": "de94bca45cfb508ec340645b2dfda86c",
+    "url": "/static/media/view_computer.de94bca4.png"
+  }
+]);
